5e2d3a2f048956e3ffa93da163fc9314611d5932
tapan0810 <arceus0810@gmail.com>
2026-02-08 09:46:12 +0530

    feat: add server-side pagination for students API

---
+++ Dotnet_API_07/Controllers/StudentPaginationController.cs
@@ -0,0 +1,26 @@
+﻿using Dotnet_API_07.Entites.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_API_07.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentPaginationController : ControllerBase
+    {
+        private readonly IStudentService _service;
+
+        public StudentController(IStudentService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedResultDto<StudentReadDto>>> GetStudents(
+            [FromQuery] PaginationDto pagination)
+        {
+            var result = await _service.GetStudents(pagination);
+            return Ok(result);
+        }
+    }
+}
--- Dotnet_API_07/Services/StudentService.cs
+++ Dotnet_API_07/Services/StudentService.cs
@@ -1,7 +1,46 @@
-﻿namespace Dotnet_API_07.Services
-{
-    public class StudentService
-    {
+﻿using Dotnet_API_07.Data;
+using Dotnet_API_07.Entites.Dtos;
+using Dotnet_API_07.Entities.Dtos;
+using Microsoft.EntityFrameworkCore;
 
+namespace Dotnet_API_07.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly StudentDbContext _context;
+
+        public StudentService(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PagedResultDto<StudentReadDto>> GetStudents(PaginationDto pagination)
+        {
+            var query = _context.Students.AsQueryable();
+
+            var totalRecords = await query.CountAsync();
+
+            var students = await query
+                .Skip((pagination.PageNumber - 1) * pagination.PageSize)
+                .Take(pagination.PageSize)
+                .Select(s => new StudentReadDto
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    PhoneNumber = s.PhoneNumber,
+                    Email = s.Email,
+                    Description = s.Description
+                })
+                .ToListAsync();
+
+            return new PagedResultDto<StudentReadDto>
+            {
+                PageNumber = pagination.PageNumber,
+                PageSize = pagination.PageSize,
+                TotalRecords = totalRecords,
+                TotalPages = (int)Math.Ceiling(totalRecords / (double)pagination.PageSize),
+                Data = students
+            };
+        }
     }
 }
